7531e27dd9f7f11cbe17429f7849315493eb513f
Kostyantyn <shtoykokostya8@gmail.com>
2025-06-08 17:08:32 +0200

    Remove config.py from repository

--- bot/config.py
+++
@@ -1,5 +0,0 @@
-# Telegram bot token
-BOT_TOKEN = '7479412412:AAEF2H0FhqOsO_N6sY_numEU12jk6YQ8fl8'
-
-# WebApp base URL (must be reachable from Telegram)
-WEBAPP_URL = 'https://09ef-188-252-198-158.ngrok-free.app'
